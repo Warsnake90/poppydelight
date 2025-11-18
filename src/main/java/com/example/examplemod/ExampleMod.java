fbaa19243f6a2e8bb0bf9ac9b87673d50a78ca4f
Warsnake90 <xd329281@gmail.com>
2025-11-18 17:26:50 -0500

    initial commit

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -28,7 +28,6 @@ import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(ExampleMod.MODID)
 public class ExampleMod
 {
